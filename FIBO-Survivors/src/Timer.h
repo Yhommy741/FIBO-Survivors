ca701115ca743d9e9f4563d4bc2577c2c5b4a2dc
_Yhommy_ <sayompoo.sinthana@gmail.com>
2025-05-28 19:42:07 +0700

    Final

--- FIBO-Survivors/src/Timer.h
+++ FIBO-Survivors/src/Timer.h
@@ -6,13 +6,14 @@
 #include <array>
 #include <optional>
 
+// Class Timer represents Survive timee in the game.
 class Timer {
 protected:
     std::filesystem::path exeDir;
     std::string fileName;
 
     sf::Texture texture;
-    std::array<std::optional<sf::Sprite>, 5> sprites; // 5 sprites for MM:SS
+    std::array<std::optional<sf::Sprite>, 5> sprites;
 
     int frameWidth = 20;
     int frameHeight = 20;
@@ -23,13 +24,20 @@ protected:
     int totalFrames = 11;  
 
 public:
+	// Constructor to initialize the Timer with a file name, executable directory, and total frames
     Timer(const std::string& fileName, const std::filesystem::path& exeDir, int totalFrames = 11);
 
+	// Initializes the Timer by loading the texture and setting up sprites
     void initialize();
     void load();
+
+	// Updates the Timer based on elapsed time and player position
     void update(float deltaTime, sf::Vector2f playerPosition);
+
+	// Draws the Timer on the given window
     void draw(sf::RenderWindow& window);
 
 protected:
+	// Converts a digit to the corresponding frame index
     int digitToFrame(int digit);
 };
